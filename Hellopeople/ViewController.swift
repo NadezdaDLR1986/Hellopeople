19385bcf5bf687f63151e6fdb9f6753511bb6d9a
NadezdaDLR1986 <145932937+NadezdaDLR1986@users.noreply.github.com>
2024-04-01 02:18:54 +0200

    Initial Commit

---
+++ Hellopeople/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hellopeople
+//
+//  Created by Nadya De Loureiro Ramos on 01/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
